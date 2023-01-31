91c71aa7b3556b72eceafabee4fd352152fa5990
Ihor Didenko <tailormoon@icloud.com>
2023-01-31 21:32:04 +0200

    ds

--- omni_P021.mk
+++ omni_P021.mk
@@ -28,6 +28,16 @@ $(call inherit-product, device/asus/P021/device.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/asus/P021/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := P021
 PRODUCT_NAME := omni_P021
